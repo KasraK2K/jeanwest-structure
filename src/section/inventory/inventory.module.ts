0b9ba1dc1ef71aea3f98322442036a9e84694f22
Maytham mohamadi <m.mohamadi@avakatan.com>
2021-01-12 14:41:45 +0330

    product controller removed

--- src/section/inventory/inventory.module.ts
+++ src/section/inventory/inventory.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
-import { ProductController } from './module/product/controller/product.controller';
 import { productProviders } from './module/product/provider/product.provider';
 
 @Module({
   imports: [DatabaseModule],
   providers: [...productProviders],
-  controllers: [ProductController],
   exports: [...productProviders],
 })
 export class InventoryModule {}
