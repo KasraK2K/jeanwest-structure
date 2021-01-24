fae5cdb6edb08a210f19a1d0c3950dfad8d9a1db
Maytham mohamadi <m.mohamadi@avakatan.com>
2021-01-24 12:54:57 +0330

    add pg to database providers

--- src/common/constant/database.const.ts
+++ src/common/constant/database.const.ts
@@ -1,7 +1,10 @@
-export const JW_DB = 'database_jeanswest';
+export const JW_TYPEORM_DB = 'typeorm_database_jeanswest';
+export const JW_PG_DB = 'pg_database_jeanswest';
 export const JW_TYPEORM_CNN = 'JEANSWEST_TYPEORM_CONNECTION';
+export const JW_PG_CNN = 'JEANSWEST_PG_CONNECTION';
 export const JW_TYPEORM_REPO = 'JEANSWEST_TYPEORM_REPOSITORY';
+export const JW_PG_REPO = 'JEANSWEST_PG_REPOSITORY';
 
-export const ERP_DB = 'database_erp';
+export const ERP_TYPEORM_DB = 'typeorm_database_erp';
 export const ERP_TYPEORM_CNN = 'ERP_TYPEORM_CONNECTION';
 export const ERP_TYPEORM_REPO = 'ERP_TYPEORM_REPOSITORY';
