9b6eded39aab0ea4ee0d8638f271a18247cb440f
Maytham mohamadi <m.mohamadi@avakatan.com>
2021-01-24 14:52:06 +0330

    inventory gateway added

---
+++ src/gateway/inventory/provider/product.provider.ts
@@ -0,0 +1,9 @@
+import { ProductService } from "src/inventory/product/service/product.service";
+import { INVENTORY_PRODUCT_SERVICE } from "../common/constant/inventory.const";
+
+export const productProviders = [
+  {
+    provide: INVENTORY_PRODUCT_SERVICE,
+    useClass: ProductService,
+  },
+];
